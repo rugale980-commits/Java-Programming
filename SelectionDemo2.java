3d6a98fa1833e04f0f09815a13684464dd3042da
Rahul Ugale <rugale980@gmail.com>
2025-10-09 10:03:59 +0530

    Add files via upload

---
+++ SelectionDemo2.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class SelectionDemo2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int istd = 0;
+
+        System.out.println("Enter your standard  : ");
+        istd = sobj.nextInt();
+
+        if(istd == 1)
+        {
+            System.out.println("Exam at 9 AM");
+        }
+        else if(istd == 2 )
+        {
+            System.out.println("Exam at 10 AM");
+        }
+        else if(istd == 3 )
+        {
+            System.out.println("Exam at 11 AM");
+        }
+        else if(istd == 4 )
+        {
+            System.out.println("Exam at 12 NOON");
+        }
+        else
+        {
+            System.out.println("Invalid standard");
+        }
+    }
+}
