61607772eeddeafdd3bbac8853567a54fe6def47
Rahul Ugale <rugale980@gmail.com>
2025-10-09 23:25:15 +0530

    Add files via upload

---
+++ RMDDemo1.java
@@ -0,0 +1,22 @@
+class Base
+{
+    public int i,j;
+
+}
+
+class Derived extends Base
+{
+    public int x;
+}
+
+class RMDDemo1
+{
+    public static void main(String A[])
+    {
+        Base bp1 = new Base();           //No Casting
+        Derived dp2 = new Derived();     //No Casting
+        Base bp2 = new Derived();        //UpCasting
+        Derived dp2 = new Base();        //DownCasting  //Error
+
+    }
+}
---
+++ Single.java
@@ -0,0 +1,32 @@
+class Base  // 8 byte
+{
+    public int i,j;
+
+    public void fun()
+    {
+        System.out.println("Inside Base fun");
+    }
+}
+
+class Derived extends Base  // 12 byte 
+{
+    public int x;
+
+    public void gun()
+    {
+        System.out.println("Inside Derived gun");
+    }
+}
+class Single
+{
+    public static void main(String A[])
+    {
+        Base bobj = new Base();
+        Derived dobj = new Derived();
+
+        bobj.fun();
+
+        dobj.fun();
+        dobj.gun();
+    }
+}
