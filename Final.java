f043fb9f2d5612575c95854b4ef9d885099ae983
Rahul Ugale <rugale980@gmail.com>
2025-10-13 13:10:46 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,11 @@
+class Final
+{
+    public static void main(String A[])
+    {
+        final int i = 10;
+      
+        i = 20;
+
+        System.out.println("i value is : "+i);
+    }
+}
---
+++ FinalClass2.java
@@ -0,0 +1,23 @@
+final class Base
+{
+    public void fun()
+    {
+        System.out.println("Inside base fun");
+    }
+}
+
+class Derived 
+{
+    public Base bobj;         // Composition
+
+    
+   
+}
+
+class FinalClass2
+{
+    public static void main(String A[])
+    {
+        
+    }
+}
