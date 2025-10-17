82540cb1d1237ed4f647dec25e9a68044f25e393
Rahul Ugale <rugale980@gmail.com>
2025-10-17 11:54:59 +0530

    Add files via upload

---
+++ FinalMethod1.java
@@ -0,0 +1,23 @@
+class Base
+{
+    public void fun()     // Defination
+   {}
+    public void fun()     // Defination
+   {}
+}
+
+class Derived extends Base
+{
+    public void run()    // Defination
+    {}
+    public void gun()    //Overriding
+    {}
+}
+
+class FinalMethod1
+{
+    public static void main(String A[])
+    {
+        Demo dobj = new Demo();
+    }
+}
